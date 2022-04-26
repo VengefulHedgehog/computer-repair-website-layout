20e079672265733b37bd13f33f7a09acb722c42b
Evgeny Kuzminykh <46488242+VengefulHedgehog@users.noreply.github.com>
2022-04-26 22:45:58 +0300

    +2 block
    +block contacts, review

---
+++ Ремонт ПК/js/script.js
@@ -0,0 +1,13 @@
+
+$(function (){
+    $('.review__scroll').slick({
+        prevArrow: '."scroll__arrow__prev',
+        nextArrow: '."scroll__arrow__next',
+        slidesToShow: 1,
+    });
+});
+
+
+
+
+
