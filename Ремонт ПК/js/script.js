4c16045417f74a542e2dffb64bd75e62039b0540
Evgeny Kuzminykh <46488242+VengefulHedgehog@users.noreply.github.com>
2022-05-03 22:28:22 +0300

    +2 blocks

--- Ремонт ПК/js/script.js
+++ Ремонт ПК/js/script.js
@@ -1,13 +1,6 @@
-
-$(function (){
-    $('.review__scroll').slick({
-        prevArrow: '."scroll__arrow__prev',
-        nextArrow: '."scroll__arrow__next',
-        slidesToShow: 1,
+$(function(){
+    $('.slider__content').slick({
+        prevArrow:'.arrow__prev',
+        nextArrow:'.arrow__next',
     });
 });
-
-
-
-
-
